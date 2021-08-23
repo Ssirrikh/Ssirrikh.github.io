293fcfcb882a97efa8f1c617d21f54f868ec7d90
Ssirrikh <nadisasmito033@g.ucla.edu>
2021-08-22 17:46:01 -0700

    add speaking dictionary

---
+++ dictionary-speaking/script/dictionary.js
@@ -0,0 +1,360 @@
+
+////////////////////////////
+//
+//
+/////////////////////////////
+
+/*
+
+Feature List:
+
+*trie
+	*nodes
+	*add/remove
+	*search
+*dictionary
+	*add entries (aka parse input)
+	*search word (primary or secondary)
+	*search fragment
+	*forEach()
+*processing
+	*alphabetize
+analytics
+	//
+
+*/
+
+///////////////////////////////////////////////////////////////////
+
+//// TRIE DATASTRUCTURE ////
+
+class Node {
+	constructor(key, value) {
+		this.key = key;
+		this.value = value;
+		this.children = [];
+	}
+
+	get childCount() {
+		return this.children.length;
+	}
+
+	hasChild(key) {
+		for (let i = 0, n = this.children.length; i < n; i++) {
+			if (this.children[i].key == key) return true;
+		}
+		return false;
+	}
+	addChild(key, value) {
+		this.children.push(new Node(key, value));
+	}
+	getChild(key) {
+		for (let i = 0, n = this.children.length; i < n; i++) {
+			if (this.children[i].key == key) return this.children[i];
+		}
+	}
+	removeChild(key) {
+		for (let i = 0, n = this.children.length; i < n; i++) {
+			if (this.children[i].key == key) {
+				this.children.splice(i, 1);
+				return;
+			}
+		}
+	}
+}
+class Trie {
+	constructor() {
+		this.root = new Node('');
+	}
+
+	addWord(word, data) {
+		let node = this.root, letter;
+		for (let i = 0, n = word.length; i < n; i++) {
+			letter = word[i];
+			if (!node.hasChild(letter)) node.addChild(letter);
+			node = node.getChild(letter);
+		}
+		node.value = data;
+	}
+	removeWord(word) {
+		if (!this.contains(word)) return;
+		this._removeHelper(word, this.root);
+	}
+	_removeHelper(word, root) {
+		// recursive deletion
+		if (word.length == 0) {
+			root.value = undefined;
+			return true;
+		}
+		if (this._removeHelper(word.substring(1), root.getChild(word[0]))) {
+			if (root.getChild(word[0]).childCount == 0) {
+				root.removeChild(word[0]);
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	_searchHelper(word) {
+		let node = this.root, contains = true,
+			i = 0, n = word.length;
+		for (; i < n; i++) {
+			if (!node.hasChild(word[i])) {
+				contains = false;
+				break;
+			} else {
+				node = node.getChild(word[i]);
+			}
+		}
+		return {
+			contains: contains,
+			value: (contains) ? node.value : undefined
+		}
+	}
+	getSearch(word)	{ return this._searchHelper(word) }
+	contains(word)	{ return this._searchHelper(word).contains }
+	search(word)	{ return this._searchHelper(word).value }
+}
+
+///////////////////////////////////////////////////////////////////
+
+//// DICTIONARY ////
+
+class Dictionary {
+	constructor(primary = {}, secondary = {}, tsv = '', parse = ()=>({})) {
+		// helper funcs
+		function comesBefore(target, reference, alphabet) {
+			if (target == reference) return false;
+			// Return true if target word comes before reference word when alphabetized.
+			// If one is a subset of the other (ex. "book" and "booked"), the shorter word comes first.
+			for (let i = 0; i < target.length && i < reference.length; i++) {
+				// check for invalid characters (not found in alphabet)
+				if (alphabet.indexOf(target[i]) == -1 || alphabet.indexOf(reference[i]) == -1) return false;
+				// if letters are the same, check next letter
+				if (target[i] == reference[i]) continue;
+				// else, there was a difference so we can tell which comes first right away
+				return (alphabet.indexOf(target[i]) < alphabet.indexOf(reference[i]));
+			}
+			// if words identical thus far and one ran out, the shorter one comes first
+			return (target.length < reference.length);
+		}
+		function alphabetize(arr, alphabet) {
+			let r = [arr[0]],
+				currentWord = '';
+			for (let wordPos = 1; wordPos < arr.length; wordPos++) {
+				currentWord = arr[wordPos];
+				let i = 0;
+				for (i = 0; i < r.length; i++) {
+					// current word should come before reference word, so stop
+					if (comesBefore(currentWord, r[i], alphabet)) break;
+				}
+				// we should have stopped on the word in r[] that comes immediately after the current word, so just splice it in
+				r.splice(i, 0, currentWord);
+			}
+			return r;
+		}
+		// init data objs
+		let p = this._primary = {};
+			p.name		= primary.name || 'Unnamed';
+			p.abbr		= (primary.abbr || p.name.substr(0,2)).toLowerCase();
+			p.alphabet	= primary.alphabet || `AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz`;
+			p.trie				= new Trie();	// todo (analytical): build 2 tries for each; one using only a single form of each word, and the other comprehensive
+			p.orderedWords		= []; // alphabetized searchable words
+			p.orderedEntries	= []; // alphabetized entries (prevents duplicate entries if multiple forms of word)
+		let s = this._secondary = {};
+			s.name		= secondary.name || 'Unnamed';
+			s.abbr		= (secondary.abbr || p.name.substr(0,2)).toLowerCase();
+			s.alphabet	= secondary.alphabet || `AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz`;
+			s.trie				= new Trie();
+			s.orderedWords		= []; // alphabetized searchable words
+			s.orderedEntries	= []; // alphabetized entries (prevents duplicate entries if multiple forms of word)
+		// settings/etc
+		this._togglePrimary = false;
+		this._trimSearch = false;
+		this._maxSearchResults = 10;
+		// process data
+		this._dataRaw = tsv;
+		let d = this._dataParsed = parse(tsv),
+			entriesBlob = {}, // use a data blob so entries access their data by reference, not value (prevent duplicate storages)
+			blobRef;
+		for (let entry of d) {
+			blobRef = entry.pTags[0];
+			entriesBlob[blobRef] = entry;
+			for (let tag of entry.pTags) {
+				p.orderedWords.push(tag);
+				p.trie.addWord(tag.toLowerCase(), entriesBlob[blobRef]); // force lowercase, for ease of searching later
+			}
+			for (let tag of entry.sTags) {
+				s.orderedWords.push(tag);
+				s.trie.addWord(tag.toLowerCase(), entriesBlob[blobRef]); // force lowercase, for ease of searching later
+			}
+			p.orderedEntries.push(entry.pTags[0]);
+			s.orderedEntries.push(entry.sTags[0]);
+		}
+		p.orderedWords = alphabetize(p.orderedWords, p.alphabet);
+		s.orderedWords = alphabetize(s.orderedWords, s.alphabet);
+		p.orderedEntries = alphabetize(p.orderedEntries, p.alphabet);
+		s.orderedEntries = alphabetize(s.orderedEntries, s.alphabet);
+	}
+
+	get primary() { return (!this._togglePrimary) ? this._primary.name : this._secondary.name; }
+	get secondary() { return (!this._togglePrimary) ? this._secondary.name : this._primary.name; }
+	get abbr() { return (!this._togglePrimary) ? (this._primary.abbr+'-'+this._secondary.abbr) : (this._secondary.abbr+'-'+this._primary.abbr); }
+
+	togglePrimary() { return (this._togglePrimary = !this._togglePrimary); }
+
+	get maxSearchResults() { return this._trimSearch ? this._maxSearchResults : -1; }
+	set maxSearchResults(n) { return this._maxSearchResults = parseInt(n); }
+	toggleTrimSearch() { return this._trimSearch = !this._trimSearch; }
+
+	searchWord(word, lang = 'primary') {
+		// sterilize input
+		word = word.toLowerCase();
+		lang = lang.toLowerCase();
+		if (lang != 'primary' && lang != 'secondary') return undefined;
+		if (this._togglePrimary) lang = (lang=='primary') ? 'secondary' : 'primary';
+		// check if word exists, and return its entry if it does
+		let search = this['_'+lang].trie.getSearch(word);
+		if (!search.contains)
+			return false;
+		else
+			return this['_'+lang].trie.search(word);
+	}
+	searchFragment(frag, lang = 'primary') {
+		// sterilize input
+		frag = frag.toLowerCase();
+		lang = lang.toLowerCase();
+		if (lang != 'primary' && lang != 'secondary') return undefined;
+		if (this._togglePrimary) lang = (lang=='primary') ? 'secondary' : 'primary';
+		// empty string returns entire dictionary
+		if (frag == '') {
+			let r = [];
+			for (let word of this['_'+lang].orderedWords) { r.push(word); }
+			return r;
+		}
+		// else, search for fragment
+		let r = [],
+			orderedWords = this['_'+lang].orderedWords,
+			lastPos = 0,
+			maxResults = this._trimSearch ? this._maxSearchResults : orderedWords.length;
+		for (let hitCount = 0; hitCount < maxResults; hitCount++) {
+			let foundMatch = false;
+			// iterate through all words in alphabetical order
+			for (; lastPos < orderedWords.length; lastPos++) {
+				// iterate through letters of current word
+				for (let i = 0; i < orderedWords[lastPos].length; i++) {
+					if (i == (frag.length-1) && orderedWords[lastPos][i].toLowerCase() == frag[i]) {
+						// current word contained entire search fragment, so add it to the list
+						r.push(orderedWords[lastPos]);
+						lastPos++;
+						foundMatch = true;
+						break;
+					} else if (orderedWords[lastPos][i].toLowerCase() != frag[i]) {
+						// there was a difference, so try next word
+						break;
+					}
+				}
+				// check if a result was found yet
+				if (foundMatch) break;
+			}
+		}
+
+		return r;
+	}
+	forEachWord(callback, reverse) {
+		let lang = (!this._togglePrimary) ? 'primary' : 'secondary';
+		for (let word of this['_'+lang].orderedWords) callback(this['_'+lang].trie.search(word.toLowerCase()), word);
+	}
+	forEachEntry(callback, lang = 'primary', reverse) {
+		lang = lang.toLowerCase();
+		if (lang != 'primary' && lang != 'secondary') return undefined;
+		if (this._togglePrimary) lang = (lang=='primary') ? 'secondary' : 'primary';
+		for (let word of this['_'+lang].orderedEntries) callback(this['_'+lang].trie.search(word.toLowerCase()));
+	}
+}
+class AnalyticalDictionary extends Dictionary {
+	constructor(primary = {}, secondary = {}, tsv = '', parse = ()=>({})) {
+		super (primary, secondary, tsv, parse);
+
+		//
+	}
+
+	_searchFragInList(frag, list) {
+		let t1 = Date.now();
+		let r = [],
+			fragLen = frag.length,
+			potentialMatches = 0;
+		// for each word, in alphabetical order
+		for (let word = 0; word < list.length; word++) {
+			let foundMatch = false;
+			// for each letter of the current word
+			for (let startLetter = 0, wordLen = list[word].length; startLetter+fragLen < wordLen; startLetter++) {
+				// compare against fragment char by char
+				for (let i = 0; i < fragLen; i++) {
+					let letter = startLetter + i;
+					if ( (i == fragLen-1) && (list[word][letter] == frag[i]) ) {
+						// if entire fragment found in current word, add it to the list
+						r.push(list[word]);
+						word++;
+						foundMatch = true;
+						break;
+					} else if (list[word][letter] != frag[i]) {
+						// there was a difference, so try next start pos
+						break;
+					} else {
+						// matched current char, so try next letter
+						if (i == 0) potentialMatches++;
+					}
+				} 
+				// check if a result was found yet
+				if (foundMatch) break;
+			}
+		}
+
+		////
+		console.log('Done in ' + (Date.now()-t1) + ' ms. Out of ' + potentialMatches + ' partial matches, ' + r.length + ' contained the entire fragment.');
+		////
+
+		return r;
+	}
+
+	searchFragmentExtended(frag, lang = 'primary') {
+		// sterilize input
+		frag = frag.toLowerCase();
+		lang = lang.toLowerCase();
+		if (lang != 'primary' && lang != 'secondary') return undefined;
+		if (this._togglePrimary) lang = (lang=='primary') ? 'secondary' : 'primary';
+
+		////
+		console.log('Searching ' + (lang=='primary'?this.primary:this.secondary) + ' for word fragment "' + frag + '"...');
+		////
+
+		// search
+		let orderedWords = this['_'+lang].orderedWords;
+		return this._searchFragInList(frag, orderedWords);
+	}
+	searchSentencesForFrag(frag, lang = 'primary') {
+		// sterilize input
+		frag = frag.toLowerCase();
+		lang = lang.toLowerCase();
+		if (lang != 'primary' && lang != 'secondary') return undefined;
+		if (this._togglePrimary) lang = (lang=='primary') ? 'secondary' : 'primary';
+
+		////
+		console.log('Searching ' + (lang=='primary'?this.primary:this.secondary) + ' sentences for word fragment "' + frag + '"...');
+		////
+
+		let sentences = [];
+		let translation = (lang=='primary') ? 'p' : 's';
+		this.forEachEntry(function(entry) {
+			for (let sentence of entry.sentences) {
+				sentences.push(sentence[translation].toLowerCase());
+			}
+		});
+		console.log(sentences);
+		return this._searchFragInList(frag, sentences);
+	}
+}
+
+
---
+++ dictionary-speaking/script/en-ch.js
@@ -0,0 +1,122 @@
+
+let primary_eng = {
+	name: 'English',
+	abbr: 'en',
+	alphabet: `AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz`
+};
+let secondary_chk = {
+	name: 'Chukchansi',
+	abbr: 'ch',
+	alphabet: `AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzʔ'`
+};
+let forms_chk = {
+	'n': ['subject', 'object', 'possessive',     'possessed', 'instrumental', 'locative'],
+	'v': ['subject', 'object', 'yesterday past', 'ongoing',   'command',      'hypothetical', 'future']
+};
+function parse_en_ch(tsv) {
+	let r = [],
+		entries = tsv.split('\n'),
+		line,
+		temp;
+	for (let entry of entries) {
+		let e = {
+			// meta info: searchable tags
+			pTags: [],
+			sTags: [],
+			// entry data: part of speech, labeled forms, sentence pairs
+			catg: '',
+			pForms: [],
+			sForms: [],
+			sentences: [],
+			// functions: for ease of searching
+			pForm: function(form) {
+				// console.log('searching primary form "' + form + '" of word "' + e.pTags[0] + '"');
+				let i;
+				for (i = 0; i < e.pForms.length; i++)
+					if (e.pForms[i].name == form) break;
+				if (i == e.pForms.length) return false;
+				return e.pForms[i];
+			},
+			sForm: function(form) {
+				// console.log('searching secondary form "' + form + '" of word "' + e.pTags[0] + '"');
+				let i;
+				for (i = 0; i < e.sForms.length; i++)
+					if (e.sForms[i].name == form) break;
+				if (i == e.sForms.length) return false;
+				return e.sForms[i];
+			}
+		};
+		line = entry.split('	');
+		// part of speech
+		e.catg = line[1];
+		// eng forms
+		let engForms = line[0].split('; ');
+		for (let i = 0; i < engForms.length; i++) {
+			e.pForms.push({name: `form`+i, word: engForms[i]});
+			e.pTags.push(engForms[i]);
+		}
+		// chk forms
+		let formNames = forms_chk[e.catg] || [];
+		for (let i = 6; i < line.length; i++) {
+			if (line[i] != '') {
+				e.sForms.push({name: formNames[i-6] || ('form'+(i-6)), word: line[i]});
+				if (i==6 || i==7) { e.sTags.push(line[i]); }
+			}
+		}
+		// sentences
+		for (let i = 2; i < 5; i += 2) {
+			if (line[i] != '' && line[i+1] != '') {
+				e.sentences.push({
+					p: line[i+1],
+					s: line[i]
+				});
+			}
+			if (line[i] == '' && line[i+1] != '' || line[i] != '' && line[i+1] == '') {
+				console.log('missing sentence translation for "' + line[0] + '"');
+			}
+		}
+
+		r.push(e);
+	}
+	return r;
+}
+
+/////////////////////////////////////////
+
+let audio_en_ch = ["baabas_H.mp3", "baabasi_H.mp3", "bokbokish_H.mp3", "bokbokshi_H.mp3", "boʔsha_H.mp3", "boʔush_H.mp3", "cheexaʔ_H.mp3", "cheexaʔan_H.mp3", "dadaach'i_H.mp3", "dadach'_H.mp3", "diwdiw_J.mp3", "gaaduʔ_H.mp3", "gaaduʔun_H.mp3", "heedin_H.mp3", "heedina_H.mp3", "k'ut'_H.mp3", "k'ut'a_H.mp3", "lopis_H.mp3", "lopso_H.mp3", "maamil'_H.mp3", "maamila_H.mp3", "maw'_J.mp3", "monosh_J.mp3", "peeli_H.mp3", "pel'_H.mp3", "saayiʔ_H.mp3", "saayiʔin_H.mp3", "shidgil'_J.mp3", "shoopin_H.mp3", "shoopina_H.mp3", "tew'_J.mp3", "weheeshit_J.mp3", "woshok'_J.mp3", "xomix_J.mp3", "xoʔ_H.mp3", "xoʔo_H.mp3", "ʔeeniʔ_H.mp3", "ʔeeniʔin_H.mp3", "ʔutuʔ_H.mp3", "ʔutuʔun_H.mp3"];
+
+let image_en_ch = ["dadach'.jpg", "gaaduʔ.jpg", "galshuy.png", "heedin.jpg", "k'ut'.jpg", "lopis.jpg", "maamil'.jpg", "monosh.png", "pel'.jpg", "saayiʔ.jpg", "shidgil'.jpg", "shoopin.jpg", "weheeshit.jpg", "woshok'.png", "xomix.jpg", "xoʔ.jpg", "ʔeeniʔ.jpg", "ʔutuʔ.jpg", "baabas.jpg", "bokbokish.jpg", "boʔush.jpg", "cheexaʔ.jpg"];
+
+let tsv_en_ch;
+
+tsv_en_ch = `potato	n	K'oteehat baabas.	The potatoes grew bigger.	Kishaalen' naʔ baabasi ʔustuubaw.	I'm going to fry the potatoes on the stove.	baabas	baabasi	baabasin	baabasam'	baabasan	baabasiw	
+hole (general)	n	Mich k'otiʔ bokbokish ʔodbinit.	A very big hole opened up.	Diʔishtaʔ naʔ bokbokshi sheleelaw.	I made a hole in the rock.	bokbokish	bokbokshi	bokbokshin	bokboksham'	bokbokshan	bokbokshiw	
+meat	n	Ch'ik'neʔ miʔin boʔush.	The meat is going to rot soon.	Kewshit naʔ boʔsha.	I boiled the meat.	boʔush	boʔsha	boʔshin	boʔsham'	boʔshan	boʔshaw	
+dog	n	Cheexaʔ yawaltaʔ noonoʔon.	The dog chased the man.	Yawaltaʔ noonoʔ cheexaʔan.	The man chased the dog.	cheexaʔ	cheexaʔan	cheexaʔan	cheexam'	cheexan	cheexaw	
+foot	n	Taxeetaxon' nim dadach'.	My feet have been aching.	Taʔashʔan' naʔ nim dadaach'i.	I'm looking at my feet.	dadach'	dadaach'i	dadaach'in	dadaach'am'	dadaach'an	dadaach'iw	
+hawk; chicken hawk	n	Hoyintaʔ diwdiw' sipin'.	The hawk flew above.	Yawaltaʔ k'otiʔ ch'enbay' diwdiwa.	A big bird chased the hawk.	diwdiw	diwdiwa	diwdiwin			diwdiwa	
+cat	n	Xishwit gaaduʔ ʔutuʔun.	The cat scratched the tree.	Yawaltaʔ cheexaʔ gaaduʔun.	The dog chased the cat.	gaaduʔ	gaaduʔun	gaaduʔun	gaadum'	gaadun	gaaduw	
+family; cousin; relative	n	ʔoyit nim heedin Pelesnow.	My family moved to Fresno.	Taʔishtaʔ naʔ ʔam heedina denderow.	I saw his family in the store.	heedin	heedina	heedinin	heedinam'		heedinaw	
+tail	n	Goosin k'ut' yuxaʔich'.	The pig's tail is curly.	Dach'taʔ naʔ ʔam k'ut'a.	I stepped on its tail.	k'ut'	k'ut'a	k'ut'in	k'ut'am'		k'ut'aw	
+fish	n	Shilit'taʔ lopis nim wakaayaw.	My fish jumped into the river.	Kuyʔulut lopso.	She salted the fish.	lopis	lopso	lopsin	lopsam'	lopsan	lopsow	
+blackberry	n	Meejintaʔ k'uyuuk'ataʔ maamil'.	The blackberries were very sweet.	Goobeʔ naʔ miʔin maamila.	I'm going to pick blackberries soon.	maamil'	maamila	maamilin	maamilam'	maamilan	maamilaw	
+grey squirrel	n	Xataʔan' maw' simiiyaʔan.	The grey squirrel is eating seeds.	Maawi naʔ wanit toonan.	I gave the grey squirrel some nuts.	maw'	maawi	maawin			maawiw	
+eight	n	Hiʔ daʔ monosh bilaasuʔ.	Here are eight plates.	Xattaʔ ʔamaʔ monoosha duldiiyaʔan.	He ate eight tortillas.	monosh	monoosha	monooshin	monoosham'	monooshan	monooshaw	
+road	n	Ch'apeeyataʔ pel'.	The road was wet.	Diʔsheʔ ʔaman peeli.	They will build the road.	pel'	peeli	peelin			peeliw	
+wing; feather	n	Limeek'ataʔ ʔam saayiʔ.	Its wings became black.	Xayaataʔ saayiʔin lameesaw.	He put down the feather on the table.	saayiʔ	saayiʔin	saayiʔin			saayiw	
+ground squirrel	n	Shidgil' ʔam waayit shoyxo.	The squirrel dug its hole.	Shidgila naʔ bokhil peeliw.	I found a squirrel on the road.	shidgil'	shidgila	shidgilin		shidgilan	shidgilaw	
+three	n	Hoy'nit shoopin ch'enbay' waʔlaw.	Three birds flew in the sky.	Hoyuch'taʔ naʔ shoopina ʔaabula.	I wanted three apples.	shoopin'	shoopina	shoopinin	shoopinam'	shoopinan	shoopinaw	
+cottontail rabbit	n	Tishtaʔ tew' ch'ayaaxiw.	The cottontail rabbit came out of the bush.	Yawaltaʔ cheexaʔ nim teewa.	My dog chased the rabbit.	tew'	teewa	teewin		teewan	teewaw	
+mountain lion	n	Gadaayit weheeshit.	The mountain lion was hungry.	Weheeshita naʔ beeletaʔ.	I fed the mountain lion.	weheeshit	weheeshita	weheeshitin			weheeshitaw	
+belt	n	Hiʔ woshok' mich dindinich'.	This belt is very thick.	Hech'eytaʔ naʔ woshook'o.	I tightened the belt.	woshok'	woshook'o	woshook'in	woshook'am'	woshook'an	woshook'ow	
+jackrabbit	n	Shilit'taʔ xomix.	The jackrabbit jumped.	Yawaalit kaʔyuʔ xomxi.	The coyote chased the jackrabbit.	xomix	xomxi	xomxin			xomxiw	
+house	n	Xoʔ nim ʔooch'iy'.	My house is nearby.	Walxon' may' ʔam xoʔo.	We are passing his house.	xoʔ	xoʔo		xoʔam'	xoʔan	xoʔow	
+snow	n	Miʔin ʔeeniʔ ch'aapit.	Then the snow melted.	Taʔishtaʔ ʔaman ʔeeniʔin.	They saw the snow.	ʔeeniʔ	ʔeeniʔin				ʔeeniw	
+tree	n	Bohloʔ miʔin galjin ʔutuʔ hew.	Many trees will soon grow here.	Halaxnit shidgil' ʔutuʔun.	The squirrel climbed the tree.	ʔutuʔ	ʔutuʔun	ʔutuʔun	ʔutum'	ʔutun	ʔutuw	`;
+
+let dictionary_en_ch = new AnalyticalDictionary(
+	primary_eng,
+	secondary_chk,
+	tsv_en_ch,
+	parse_en_ch
+);
